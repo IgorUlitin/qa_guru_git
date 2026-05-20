ce7d9f181e8fd82d06a1aaddaae019db9e7ba1e1
Igor Ulitin <igorulitin25@gmail.com>
2026-05-20 22:11:38 +0400

    added getMessage() method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello qa_guru");
+        System.out.printf("Hello qa_guru!");
     }
 
     private static String getMessage() {
