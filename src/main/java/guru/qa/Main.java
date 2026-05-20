25145d3b2428e9f37c220de58e8331523ee8f36a
Igor Ulitin <115343794+IgorUlitin@users.noreply.github.com>
2026-05-20 21:57:36 +0400

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,10 @@
 package guru.qa;
 
 public class Main {
+
+    private static String msg = "Hello qa_guru!!!";
+    
     public static void main(String[] args) {
-        System.out.printf("Hello qa_guru!!!");
+        System.out.printf(msg);
     }
 }
